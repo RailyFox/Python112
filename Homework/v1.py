27ce4cb9e0de7bd7d314fd94f012b913d94ce93f
RailyFox <dragolen@mail.ru>
2022-02-05 12:01:52 +0300

    add chgs

--- Homework/v1.py
+++ Homework/v1.py
@@ -1,41 +1,42 @@
-import math
+# import math
+#
+# print('\t\tЗадание 1')
+# def rectang(a, b):
+#     s = a * b
+#     return s
+#
+#
+# def triang(a, h):
+#     s = 1 / 2 * a * h
+#     return s
+#
+#
+# def circle(r):
+#     s = round(math.pi, 2) * r ** 2
+#     return s
+#
+#
+# print('Площадь какой фигуры вы хотели бы найти?')
+# ch = int(input('1 - прямоугольник, 2 - треугольник, 3 - круг: '))
+# match ch:
+#     case 1:
+#         a, b = int(input('Введите длину: ')), int(input('Введите ширину: '))
+#         print(rectang(a, b))
+#     case 2:
+#         a, h = int(input('Введите основание: ')), int(input('Введите высоту: '))
+#         print(triang(a, h))
+#     case 3:
+#         r = int(input('радиус круга: '))
+#         print(circle(r))
 
-print('\t\tЗадание 1')
-def rectang(a, b):
-    s = a * b
-    return s
-
-
-def triang(a, h):
-    s = 1 / 2 * a * h
-    return s
-
-
-def circle(r):
-    s = round(math.pi, 2) * r ** 2
-    return s
-
-
-print('Площадь какой фигуры вы хотели бы найти?')
-ch = int(input('1 - прямоугольник, 2 - треугольник, 3 - круг: '))
-match ch:
-    case 1:
-        a, b = int(input('Введите длину: ')), int(input('Введите ширину: '))
-        print(rectang(a, b))
-    case 2:
-        a, h = int(input('Введите основание: ')), int(input('Введите высоту: '))
-        print(triang(a, h))
-    case 3:
-        r = int(input('радиус круга: '))
-        print(circle(r))
-
+import random
 print('\t\tЗадание 2')
 def first_func(x):
     prime_num = []
     num = []
     for j in x:
         k = 0
-        for i in range(2, j // 2 + 1):
+        for i in range(2, abs(j) // 2 + 1):
             if (j % i == 0):
                 k += 1
         if (k <= 0 and j != 1):
@@ -46,5 +47,9 @@ def first_func(x):
 
 
 lst = [6, 3, 8, 5, 7, 9, 3, 6, 5, 13, 1]
+test_lst = [random.randint(-20, 20) for i in range(10)]
 
 first_func(lst)
+print(test_lst)
+first_func(test_lst)
+
