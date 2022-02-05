c94b48b7594ba08b82fdc2ec18dc5fb0b81490fb
RailyFox <dragolen@mail.ru>
2022-02-05 11:42:46 +0300

    save homework

--- Homework/v1.py
+++ Homework/v1.py
@@ -1,6 +1,6 @@
 import math
 
-
+print('\t\tЗадание 1')
 def rectang(a, b):
     s = a * b
     return s
@@ -28,3 +28,23 @@ match ch:
     case 3:
         r = int(input('радиус круга: '))
         print(circle(r))
+
+print('\t\tЗадание 2')
+def first_func(x):
+    prime_num = []
+    num = []
+    for j in x:
+        k = 0
+        for i in range(2, j // 2 + 1):
+            if (j % i == 0):
+                k += 1
+        if (k <= 0 and j != 1):
+            prime_num.append(j)
+        else:
+            num.append(j)
+    print('Min: ', min(prime_num), '\nMax: ', max(num))
+
+
+lst = [6, 3, 8, 5, 7, 9, 3, 6, 5, 13, 1]
+
+first_func(lst)
